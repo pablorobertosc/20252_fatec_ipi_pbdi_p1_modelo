5eedf5f4ffda810ef084c517a66324c4420646ad
Pablo <pabloroberto.s.c@gmail.com>
2025-10-07 11:19:18 -0300

    feat: Cria tabela para armazenar dados de vinhos

--- script.sql
+++ script.sql
@@ -1,6 +1,25 @@
 -- ----------------------------------------------------------------
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
+-- Criar a tabela resultados_paises se não existir
+CREATE TABLE IF NOT EXISTS resultados_paises(
+   wine_id SERIAL PRIMARY KEY,
+   csv_index INT,
+   country VARCHAR(100),
+   description TEXT,
+   designation VARCHAR(255),
+   points INT,
+   price NUMERIC(10,2),
+   province VARCHAR(100),
+   region_1 VARCHAR(100),
+   region_2 VARCHAR(100),
+   taster_name VARCHAR(100),
+   taster_twitter_handle VARCHAR(100),
+   title TEXT,
+   variety VARCHAR(100),
+   winery VARCHAR(200)
+);
+
 
 
 -- ----------------------------------------------------------------
